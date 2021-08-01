b55922eec0d2eedb0c24158c6f07fbb598db49fc
Yiming Li <yimingli.cs@gmail.com>
2021-08-01 23:36:12 +0800

    fix: add missing @vue/compiler-sfc dep for script setup

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,27 +1,27 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-    {
-		path: '/',
-		name: 'Index',
-		component: () => import('../views/Index.vue')
-	},
-	{
-		path: "/about",
-		name: "About",
-		component: () => import("../views/About.vue")
-	},
-	{
-		path: "/example",
-		name: "Example",
-		component: () => import("../views/Example.vue")
-	}
-]
+  {
+    path: "/",
+    name: "Index",
+    component: () => import("../views/Index.vue"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/example",
+    name: "Example",
+    component: () => import("../views/Example.vue"),
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
 // router.beforeEach((to, from, next) => {
 // 	next()
