25bd47bf767b6a30c593eb8aa185db81e17b9c71
Yiming Li <yimingli.cs@gmail.com>
2021-08-02 15:32:14 +0800

    feat: add element-plus

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
 
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
+
 const app = createApp(App);
 
 app.use(router);
+app.use(ElementPlus, { size: "mini" });
 app.mount("#app");
--- frontend/src/views/Example.vue
+++ frontend/src/views/Example.vue
@@ -1,6 +1,10 @@
 <template>
   <div>Exmaple</div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-pagination background layout="prev, pager, next" :total="1000">
+  </el-pagination>
+  <br />
+  <el-button type="primary" round> changeLanguage </el-button>
 </template>
 
 <script setup lang="ts">
