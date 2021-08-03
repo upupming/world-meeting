adc0c4beccc781f42bb447df25e437fd94325e78
Yiming Li <yimingli.cs@gmail.com>
2021-08-03 09:41:34 +0800

    feat: add dracula theme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/img/logo.png" />
   <div id="nav">
     <router-link to="/"> Home </router-link> |
     <router-link to="/about"> About </router-link> |
@@ -17,6 +17,8 @@ export default defineComponent({
 </script>
 
 <style lang="less">
+@import "./assets/less/common.less";
+
 html,
 body,
 #app,
