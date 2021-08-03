70e77339445ce1bbb0133659b881eaafd3992f4d
Yiming Li <yimingli.cs@gmail.com>
2021-08-03 14:43:52 +0800

    fix: clean unneeded code

--- frontend/src/stories/Button.vue
+++ frontend/src/stories/Button.vue
@@ -35,47 +35,4 @@ const style = computed(() => ({
 const onClick = () => {
   emit("click");
 };
-// export default {
-//   name: "my-button",
-
-//   props: {
-//     label: {
-//       type: String,
-//       required: true,
-//     },
-//     primary: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     size: {
-//       type: String,
-//       validator: function (value: string): boolean {
-//         return ["small", "medium", "large"].indexOf(value) !== -1;
-//       },
-//     },
-//     backgroundColor: {
-//       type: String,
-//     },
-//   },
-
-//   emits: ["click"],
-
-//   setup(props, { emit }) {
-//     props = reactive(props);
-//     return {
-//       classes: computed(() => ({
-//         "storybook-button": true,
-//         "storybook-button--primary": props.primary,
-//         "storybook-button--secondary": !props.primary,
-//         [`storybook-button--${props.size || "medium"}`]: true,
-//       })),
-//       style: computed(() => ({
-//         backgroundColor: props.backgroundColor,
-//       })),
-//       onClick() {
-//         emit("click");
-//       },
-//     };
-//   },
-// };
 </script>
