49af3f12046bbbb0c3ff98f03739269aa4eabbe4
Yiming Li <yimingli.cs@gmail.com>
2021-08-03 14:40:46 +0800

    feat: ui of avatar card

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -6,5 +6,23 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        },
+        {
+          loader: 'less-loader'
+        },
+      ],
+    });
+
+    return config;
+  },
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,6 @@ import App from "./App.vue";
 
 import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
-import "font-awesome/css/font-awesome.min.css";
 
 const app = createApp(App);
 
